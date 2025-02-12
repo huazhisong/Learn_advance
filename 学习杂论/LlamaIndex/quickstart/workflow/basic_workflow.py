112d5779dff855a176a9be3fbc8d6b8c165bf8a7
hzsong <hzsong@outlook.com>
2025-02-12 23:16:43 +0800

    workflow 20250212

---
+++ 学习杂论/LlamaIndex/quickstart/workflow/basic_workflow.py
@@ -0,0 +1,25 @@
+from llama_index.core.workflow import (
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+)
+
+
+class MyWorkflow(Workflow):
+    @step
+    async def my_step(self, ev: StartEvent) -> StopEvent:
+        # do something here
+        return StopEvent(result="Hello, world!")
+
+
+async def main():
+    w = MyWorkflow(timeout=10, verbose=False)
+    result = await w.run()
+    print(result)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
